2e636e876135111893b901acb21fc1f3b48f335a
Lur <minecraft2314@gmail.com>
2019-09-09 10:49:44 +1200

    Initial commit (v2)

---
+++ server/index.ts
@@ -0,0 +1,17 @@
+import express from 'express'
+
+// Any code run here is not seen by the client, and thus
+// authentication with secret keys, etc. is safe here.
+// After the first fetch from the server, the result is
+// cached on the client.
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.json({ it: 'works!' })
+})
+
+module.exports = {
+    path: '/api',
+    handler: app
+}
